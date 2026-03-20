6235c5515e3db3db7c0fe858d82b0468f538f672
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-03-20 13:29:28 +0300

    Update_Sedysheva_alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
+#include <iostream>
+#include "alg.h"
 
 int gcd(int a, int b) {
-	return 25;
+    while (b != 0) {
+        int vremPerem = b;
+        b = a % b;
+        a = vremPerem;
+    }
+    return a;
+}
+
+int main() {
+    std::cout << "NOD(48, 18) = " << gcd(48, 18) << std::endl;
+    return 0;
 }
